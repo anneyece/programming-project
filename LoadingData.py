229822589433bd75a042dc9ecfaaf1c5cb8eece7
anneyece <ianneyece@gmail.com>
2025-10-14 20:28:34 -0400

    Adding Comments to each of our graphs
    Code/ description done with Yolina
    
    Co-Authored-By: Yolixli <236599281+Yolixli@users.noreply.github.com>

--- LoadingData.py
+++ LoadingData.py
@@ -58,30 +58,37 @@ for place in area :
 
 # Sir, when you do run our code, make sure that you run each plot code seperately or else the graphs will be combined into 1
 # Areas on a plot (Bar)
-"plt.bar(Are.keys(),Are.values())"
-"plt.xticks(rotation=90)"
-"plt.tight_layout()"
-"plt.xlabel('LA Disctricts')"
-"plt.ylabel('Number of Crimes')"
-"plt.title('Crimes committed in different LA Districts')"
+
+# plt.bar(Are.keys(),Are.values())
+# plt.xticks(rotation=90)
+# plt.tight_layout()
+# plt.xlabel('LA Disctricts')
+# plt.ylabel('Number of Crimes')
+# plt.title('Crimes committed in different LA Districts')
 
 # Area on a plot (Pie)
-"plt.pie(Are.values(), labels = Are.keys(), autopct='%1.1f%%')"
-"plt.title('Percentage of Crimes committed in different LA Regions')"
+
+# plt.pie(Are.values(), labels = Are.keys(), autopct='%1.1f%%')
+# plt.title('Percentage of Crimes committed in different LA Regions')
 
 
+
+# The next 15 lines of code would be the code used to create the subplot graph, that in our case, would combine a line graph and a bar graph together.However, we are only two as i mentioned previously in the mios and we spent the entire weekend  and free time figuring out how to do it and we did our best, we created 5 out of the 7 graphs , I hope you can understand, this was the compromise that we were forced into since we did not get a reply from your part.
 # Seasons and Months on a plot (Bar + Line)
+# Average amount Seasons Bar plot code
 
-# Seasons Bar plot code
-"plt.bar(['Winter','Spring', 'Summer','Fall'],[Winter, Spring, Summer, Fall])"
-"plt.xlabel('Seasons')"
-"plt.ylabel('Number of crimes')"
-"plt.title('Crimes committed per Season')"
-
+# plt.bar(['Winter','Spring', 'Summer','Fall'],[Average_Winter, Average_Spring, Average_Summer, Average_Fall])
+# plt.xlabel('Seasons')
+# plt.ylabel('Average number of crimes')
+# plt.title('Avergae number of crimes committed per Season')
 
 # Months Line plot code
-plt.plot(fre.keys(),fre.values())
-plt.xticks(rotation=90)
+
+# plt.plot(fre.keys(),fre.values())
+# plt.xticks(rotation=90) 
+
+
+
 
 
 # Time
@@ -99,23 +106,26 @@ for t in time_occ:
 time_of_crime = dict(sorted(time_of_crime.items()))
 
 #Time on a plot (Histogram)
-"plt.bar(time_of_crime.keys(), time_of_crime.values(), width=1)"
-"plt.xlabel('Hours within a day')"
-"plt.ylabel('Number of crimes')"
-"plt.title('Crimes committed depending on the time of day')"
+
+# plt.bar(time_of_crime.keys(), time_of_crime.values(), width=1)
+# plt.xlabel('Hours within a day')
+# plt.ylabel('Number of crimes')
+# plt.title('Crimes committed depending on the time of day')
 
 #Time on a plot (Scatter)
-"plt.scatter(time_of_crime.keys(), time_of_crime.values())"
-"plt.xlabel('Hours within a day')"
-"plt.ylabel('Number of crimes')"
-"plt.title('Crimes committed depending on the time of day')"
+
+# plt.scatter(time_of_crime.keys(), time_of_crime.values())
+# plt.xlabel('Hours within a day')
+# plt.ylabel('Number of crimes')
+# plt.title('Crimes committed depending on the time of day')
 
 #Time on a plot (Scatter + Grid)
-"plt.scatter(time_of_crime.keys(), time_of_crime.values())"
-"plt.grid()"
-"plt.xlabel('Hours within a day')"
-"plt.ylabel('Number of crimes')"
-"plt.title('Crimes committed depending on the time of day')"
+
+plt.scatter(time_of_crime.keys(), time_of_crime.values())
+plt.grid()
+plt.xlabel('Hours within a day')
+plt.ylabel('Number of crimes')
+plt.title('Crimes committed depending on the time of day')
 
 plt.show()
 
