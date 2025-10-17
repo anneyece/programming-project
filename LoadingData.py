443fa23cf934c3a370beda30be7bf37f0b6c9878
anneyece <ianneyece@gmail.com>
2025-10-17 09:39:35 -0400

    More comments and reorganized lines of code
    Done with Yolina
    
    Co-Authored-By: Yolixli <236599281+Yolixli@users.noreply.github.com>

--- LoadingData.py
+++ LoadingData.py
@@ -57,36 +57,40 @@ for place in area :
 
 
 # Sir, when you do run our code, make sure that you run each plot code seperately or else the graphs will be combined into 1
-# Areas on a plot (Bar)
 
-# plt.bar(Are.keys(),Are.values())
-# plt.xticks(rotation=90)
-# plt.tight_layout()
-# plt.xlabel('LA Disctricts')
-# plt.ylabel('Number of Crimes')
-# plt.title('Crimes committed in different LA Districts')
+# Seasons on a plot (Bar)
+plt.bar(['Winter','Spring', 'Summer','Fall'],[Winter, Spring, Summer, Fall]) # The individual listed strings of each season represented the x values and the Variable names of the seasons allowed access to the number of crimes committed per those seasons and was therefore the values in our Y axis. 
+plt.xlabel('Seasons') # Title of X axis
+plt.ylabel('Total number of crimes') # Title of Y axis
+plt.title('Total number of crimes committed per Season') # Title of Graph 
+
+# Areas on a plot (Bar)
+plt.bar(Are.keys(),Are.values()) # First line of code represents our x and y axes that we chose for the following bar graph being accessed from a dictionnary Keys, the areas for the x values and the values as in the number of crimes being the y values
+plt.xticks(rotation=90) # This rotates the x values (areas) so that it does not overlap and can easily be read 
+plt.tight_layout() # Makes sure that the names do not for sure overlap 
+plt.xlabel('LA Disctricts') # title of X axis 
+plt.ylabel('Number of Crimes') # Title of Y axis
+plt.title('Crimes committed in different LA Districts') # Title of Graph 
 
 # Area on a plot (Pie)
-
-# plt.pie(Are.values(), labels = Are.keys(), autopct='%1.1f%%')
-# plt.title('Percentage of Crimes committed in different LA Regions')
-
-
+plt.pie(Are.values(), labels = Are.keys(), autopct='%1.1f%%') # First line of code represents our x and y axes that we chose for the following pie graph accessed from dictionnary and the autopct dosplays the percentage of each slice of pie allowing for more precision
+plt.tight_layout() # Makes sure that the names do not for sure overlap 
+plt.title('Percentage of Crimes committed in different LA Regions') # Title of the Graph since axis do not represnt correctly due to the form of a pie graph that is why we did not include them 
 
 # The next 15 lines of code would be the code used to create the subplot graph, that in our case, would combine a line graph and a bar graph together.However, we are only two as i mentioned previously in the mios and we spent the entire weekend  and free time figuring out how to do it and we did our best, we created 5 out of the 7 graphs , I hope you can understand, this was the compromise that we were forced into since we did not get a reply from your part.
 # Seasons and Months on a plot (Bar + Line)
 # Average amount Seasons Bar plot code
-
-# plt.bar(['Winter','Spring', 'Summer','Fall'],[Average_Winter, Average_Spring, Average_Summer, Average_Fall])
-# plt.xlabel('Seasons')
-# plt.ylabel('Average number of crimes')
-# plt.title('Avergae number of crimes committed per Season')
+plt.bar(['Winter','Spring', 'Summer','Fall'],[Average_Winter, Average_Spring, Average_Summer, Average_Fall]) 
+plt.xlabel('Seasons') # Title of X axis
+plt.ylabel('Average number of crimes') # Title of Y axis
+plt.title('Avergae number of crimes committed per Season')
 
 # Months Line plot code
 
-# plt.plot(fre.keys(),fre.values())
-# plt.xticks(rotation=90) 
+plt.plot(fre.keys(),fre.values())
+plt.xticks(rotation=90) 
 
+#COMMENT: This was the 6th graph intended to plot however, was not included as explained. In this graph, we were planning on displaying one singular graph which would be the result of two subplots being in our case, a bar graph represneting the (Average crimes per Seasons) and a line graph represneting (Crimes per Months) hoping that this combination would create a graph displaying the two as one.
 
 
 
@@ -106,28 +110,26 @@ for t in time_occ:
 time_of_crime = dict(sorted(time_of_crime.items()))
 
 #Time on a plot (Histogram)
-
-# plt.bar(time_of_crime.keys(), time_of_crime.values(), width=1)
-# plt.xlabel('Hours within a day')
-# plt.ylabel('Number of crimes')
-# plt.title('Crimes committed depending on the time of day')
+plt.bar(time_of_crime.keys(), time_of_crime.values(), width=1) #First line of code represents our x and y axes that we chose for the following bar graph. We originally attempted to us the histogram function but since we already had maniplulated the data, it was no longer a raw set but it was linked to crimes, it did not work and we needed to use the bar function instead making it resemble a typical histogram by using width= 1 to thicken and closen the bars up
+plt.xlabel('Hours within a day') # title of X axis 
+plt.ylabel('Number of crimes') # title of Y axis 
+plt.title('Crimes committed depending on the time of day') # Title of Graph 
 
 #Time on a plot (Scatter)
-
-# plt.scatter(time_of_crime.keys(), time_of_crime.values())
-# plt.xlabel('Hours within a day')
-# plt.ylabel('Number of crimes')
-# plt.title('Crimes committed depending on the time of day')
+plt.scatter(time_of_crime.keys(), time_of_crime.values()) # First line of code represents our x and y axes that we chose for the following scatter graph being accessed from a dictionnary Keys, X values being the different times/ hours of the day and the Y values being the Crimes committed per those times.
+plt.xlabel('Hours within a day') # Title of X axis
+plt.ylabel('Number of crimes') # Title of Y axis
+plt.title('Crimes committed depending on the time of day') # Title of Graph 
 
 #Time on a plot (Scatter + Grid)
 
-plt.scatter(time_of_crime.keys(), time_of_crime.values())
-plt.grid()
-plt.xlabel('Hours within a day')
-plt.ylabel('Number of crimes')
-plt.title('Crimes committed depending on the time of day')
+plt.scatter(time_of_crime.keys(), time_of_crime.values()) # First line of code represents our x and y axes that we chose for the following scatter graph being accessed from a dictionnary Keys, X values being the different times/ hours of the day and the Y values being the Crimes committed per those times.
+plt.grid() # Add a grid
+plt.xlabel('Hours within a day') # Title of X axis
+plt.ylabel('Number of crimes') # Title of Y axis
+plt.title('Crimes committed depending on the time of day') # Title of Graph 
 
-plt.show()
+plt.show() # Displays the graph(s)
 
 
 
